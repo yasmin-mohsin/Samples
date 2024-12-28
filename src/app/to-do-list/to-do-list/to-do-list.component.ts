3266428e96f0ba6750e41b7b885bda053594f0b7
yasmin-mohsin <yasminmohsin60@gmail.com>
2024-12-29 02:33:04 +0400

    implement search in task list

--- src/app/to-do-list/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list/to-do-list.component.ts
@@ -9,32 +9,27 @@ import { HttpService } from 'src/app/Shared/Services/http.service';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
-  taskList: Array<any> = []
-  priorities!: any;
+  taskList: Array<any> = [];
   showDescription: boolean = false;
-  searchValue!:any;
+  searchValue!: any;
 
   constructor(public dialog: MatDialog, private http: HttpService, private cdr: ChangeDetectorRef) {
     this.getTasks();
-    this.getPriorities();
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-
+  // USAGE => open popup and after close it update taskList with the added new
   openDialog(): void {
     const dialogRef = this.dialog.open(PopUpComponent, {
       width: '350px',
-      data: { name: 'ffff' }
     });
-
     dialogRef.afterClosed().subscribe(res => {
       this.getTasks()
     })
   }
 
-  // USAGE => use for get all the tasks and add needed properties
+  // USAGE => use for get all the tasks from api and add needed properties
   getTasks() {
     this.http.get('Tasks').subscribe((res: any) => {
       if (res) {
@@ -45,37 +40,24 @@ export class ToDoListComponent implements OnInit {
           res[ele]['id'] = ele;
           this.taskList.push(res[ele])
         })
-        console.log(this.taskList)
-
       }
     })
   }
 
-  getPriorities() {
-    this.http.get('Priority').subscribe(res => {
-      if (res) {
-        this.priorities = res;
-      }
-    })
-  }
-
+  // USAGE => use to filter task list while searching
   filterTaskList(name: any) {
     this.taskList.map(ele => {
-      if (!ele['taskName'].includes(name)) {
-        ele['show'] = false;
+      if (ele['taskName'].includes(name)) {
+        ele['show'] = true;
       }
-      else { ele['show'] = true }
+      else { ele['show'] = false }
     })
-    console.log(this.searchValue)
-    console.log(this.taskList)
   }
 
   // USAGE => use to delete task from api
   deleteTask(id: any, index: any) {
     this.http.delete(id, 'Tasks').subscribe(res => {
-      this.getTasks()
-      // this.taskList.splice(this.taskList[index], 1)
-      // console.log(this.taskList)
+      this.getTasks();
     })
   }
 
