24734c485dbff18b53dd7b02fbcf65d2f8352bb5
yasmin-mohsin <yasminmohsin60@gmail.com>
2025-01-03 05:08:12 +0400

    implement category page

--- src/app/shopping/category/category.component.ts
+++ src/app/shopping/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from 'src/app/Shared/Services/http.service';
+import { ShoppingCartService } from '../shared/Services/shopping-cart.service';
 
 @Component({
   selector: 'app-category',
@@ -11,20 +12,24 @@ export class CategoryComponent implements OnInit {
   categoryType!: string;
   products!: any;
 
-  constructor(private rout: ActivatedRoute, private http: HttpService) {
+  constructor(private rout: ActivatedRoute, private http: HttpService, private shopCart: ShoppingCartService) {
     rout.queryParams.subscribe(res => {
       this.categoryType = res['type']
       this.getProductList();
     });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-
+  // USAGE => get product list according to type from api
   getProductList() {
     this.http.get(`categories/${this.categoryType}`).subscribe(response => {
       this.products = response;
     })
+  }
+
+  // USAGE => use to add new item to cart and local storage
+  addToCart(item: any) {
+    this.shopCart.addToCart(item);
   }
 }
