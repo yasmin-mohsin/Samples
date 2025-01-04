81d16e5a7d50ecb0b944ae07ba2530e927e29e71
yasmin-mohsin <yasminmohsin60@gmail.com>
2025-01-04 04:48:37 +0400

    implement product page

--- src/app/shopping/product/product.component.ts
+++ src/app/shopping/product/product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from 'src/app/Shared/Services/http.service';
+import { ShoppingCartService } from '../shared/Services/shopping-cart.service';
 
 @Component({
   selector: 'app-product',
@@ -8,14 +9,29 @@ import { HttpService } from 'src/app/Shared/Services/http.service';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  productId: any;
+  type: any;
+  productData: any;
 
-  constructor(private rout: ActivatedRoute, private http: HttpService) {
-      rout.queryParams.subscribe(res => {
-        console.log(res)
-      });
-    }
-
-  ngOnInit(): void {
+  constructor(private rout: ActivatedRoute, private http: HttpService, private shopCart: ShoppingCartService) {
+    rout.queryParams.subscribe(res => {
+      this.productId = Number(res['index'])
+      this.type = res['type']
+    });
+    this.getProductData()
   }
 
+  ngOnInit(): void { }
+
+  // USAGE => get product data form local storage
+  getProductData() {
+    this.http.get(`categories/${this.type}/${this.productId}`).subscribe(response => {
+      this.productData = response;
+    })
+  }
+
+  // USAGE => use to add this selected item to cart throw local storage
+  addToCart() {
+    this.shopCart.addToCart(this.productData);
+  }
 }
